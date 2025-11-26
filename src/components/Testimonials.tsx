64a24cebbb6765ad00f0062d04a6b6b44fd1a752
Davi Lucas <davi.lucas9@hotmail.com>
2025-11-25 21:54:22 -0300

    Marca d'agua e tam texto

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -7,74 +7,35 @@ const Testimonials = () => {
         </h2>
         
         <div className="max-w-6xl mx-auto">
-          <div
-  className="padlet-embed"
-  style={{
-    border: '1px solid rgba(0,0,0,0.1)',
-    borderRadius: '2px',
-    boxSizing: 'border-box',
-    overflow: 'hidden',
-    position: 'relative',
-    width: '100%',
-    background: '#F4F4F4'
-  }}
->
-  <p style={{ padding: 0, margin: 0 }}>
-    <iframe
-      src="https://padlet.com/embed/6r1rdp2ckyvkqyfp/slideshow?autoplay=1&loop=1&duration=2"
-      frameBorder="0"
-      allow="clipboard-write"
-      style={{
-        width: '100%',
-        height: '608px',
-        display: 'block',
-        padding: 0,
-        margin: 0
-      }}
-    />
-  </p>
-
+          <div className="max-w-6xl mx-auto">
   <div
+    className="padlet-embed"
     style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'end',
-      margin: 0,
-      height: '28px'
+      border: '1px solid rgba(0,0,0,0.1)',
+      borderRadius: '2px',
+      boxSizing: 'border-box',
+      overflow: 'hidden',
+      position: 'relative',
+      width: '100%',
+      background: '#F4F4F4',
     }}
   >
-    <a
-      href="https://padlet.com?ref=embed"
-      style={{
-        display: 'block',
-        flexGrow: 0,
-        margin: 0,
-        border: 'none',
-        padding: 0,
-        textDecoration: 'none'
-      }}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <img
-          src="https://padlet.net/embeds/made_with_padlet_2022.png"
-          width="114"
-          height="28"
-          style={{
-            padding: 0,
-            margin: 0,
-            background: 'transparent',
-            border: 'none',
-            boxShadow: 'none'
-          }}
-          alt="Criado com o Padlet"
-        />
-      </div>
-    </a>
+    <p style={{ padding: 0, margin: 0 }}>
+      <iframe
+        src="https://padlet.com/embed/6r1rdp2ckyvkqyfp/slideshow?autoplay=1&loop=1&duration=2"
+        frameBorder="0"
+        allow="clipboard-write"
+        style={{
+          width: '100%',
+          height: '608px',
+          display: 'block',
+          padding: 0,
+          margin: 0,
+        }}
+      />
+    </p>
   </div>
 </div>
-
         </div>
       </div>
     </section>
